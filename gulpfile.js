8f719bbad4a31c52ebd2936599fc05d239f89ad8
Freddy Montes <freddymontes@gmail.com>
2016-11-09 17:51:30 -0600

    Add task for update css in dotcms instance

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,79 @@ var compass = require('gulp-compass');
 var connect = require('gulp-connect');
 var cors = require('cors');
 
-gulp.task('webserver', function() {
-  connect.server({
-    livereload: true,
-    port: 9000,
-    middleware: function() {
-      return [cors()];
+var dotcmsBaseUrl = '/Users/fmontes/dev/dotcms/';
+
+var dotcms = {
+    core : {
+        base: dotcmsBaseUrl + 'core/',
+        css: 'dotCMS/src/main/webapp/html/css/dijit-dotcms'
+    },
+    tomcat: {
+        base: dotcmsBaseUrl + 'tomcat8/',
+        css: 'webapps/ROOT/html/css/dijit-dotcms'
     }
-  });
+};
+
+gulp.task('webserver', function () {
+    connect.server({
+        livereload: true,
+        port: 9000,
+        middleware: function () {
+            return [cors()];
+        }
+    });
 });
 
-gulp.task('compass', function() {
-  gulp.src('scss/**/*.scss')
-    .pipe(compass({
-      config_file: './config.rb',
-      css: 'css',
-      sass: 'scss',
-      sourcemap: true
-    }))
-    .on('error', function(err) {})
-    .pipe(connect.reload());
+gulp.task('compass', function () {
+    gulp.src('scss/**/*.scss')
+        .pipe(compass({
+            config_file: './config.rb',
+            css: 'css',
+            sass: 'scss',
+            sourcemap: true
+        }))
+        .on('error', function (err) {})
+        .pipe(connect.reload());
 });
 
-gulp.task('reload', function() {
-  gulp.src('*.html')
-    .pipe(connect.reload());
+gulp.task('reload', function () {
+    gulp.src('*.html')
+        .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('scss/**/*.scss',['compass']);
-  gulp.watch('*.html',['reload']);
+gulp.task('watch', function () {
+    gulp.watch('scss/**/*.scss', ['compass']);
+    gulp.watch('*.html', ['reload']);
 });
 
-gulp.task('dotcms', function() {
-  gulp.src('scss/dotcms.scss')
-    .pipe(compass({
-      config_file: './config.rb',
-      css: '/Volumes/Macinstosh\ HD/Users/fmontes/dev/dotcms/core/dotCMS/html/css/',
-      sass: 'scss',
-      sourcemap: false,
-      environment: 'development'
-    }))
+gulp.task('deploy', function () {
+    gulp.src('scss/dotcms.scss')
+        .pipe(compass({
+            config_file: './config.rb',
+            css: 'css',
+            sass: 'scss',
+            sourcemap: false,
+            environment: 'production'
+        }))
+        .pipe(gulp.dest(dotcms.core.base + dotcms.core.css));
 });
+
+gulp.task('watch-dotcms', function () {
+    gulp.watch('scss/**/*.scss', function () {
+        gulp.src('scss/dotcms.scss')
+            .pipe(compass({
+                config_file: './config.rb',
+                css: 'css',
+                sass: 'scss',
+                sourcemap: false,
+                environment: 'development'
+            }))
+            .on('error', function (err) {})
+            .pipe(connect.reload())
+            .pipe(gulp.dest(dotcms.tomcat.base + dotcms.tomcat.css));
+    });
+});
+
+gulp.task('dev', ['compass', 'webserver', 'watch-dotcms']);
 
 gulp.task('default', ['compass', 'webserver', 'watch']);
